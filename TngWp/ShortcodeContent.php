a5537457880358055554e5ac7d879c8822c78153
upavadi <mahesh@upavadi.net>
2022-05-17 15:53:04 +0100

    Updating Content

--- TngWp/ShortcodeContent.php
+++ TngWp/ShortcodeContent.php
@@ -36,260 +36,205 @@ class TngWp_ShortcodeContent
  * Add shortcodes
 */
 
-public function addShortcode(TngWp_Shortcode_AbstractShortcode $shortcode)
-{
-    $this->shortcodes[] = $shortcode;
-}
-
-/**
- * Init Variables
-*/    
-public function initPlugin()
-{
-    $templates = new TngWp_Templates();
-    foreach ($this->shortcodes as $shortcode) {
-        $shortcode->init($this, $templates);
-    }
-}
-
-public function getConfigPath() {
-    $configPath = ($this->getTngPath()) . DIRECTORY_SEPARATOR . "config.php";
-    //$configPath = $tngPath . DIRECTORY_SEPARATOR . "config.php";
-    if (!file_exists($configPath)) {
-       //throw new DomainException('Could not find TNG config file');
-       $e = new DomainException('TNG Path not found');
-       error_log($e->getMessage());
-       error_log($e->getTraceAsString());
-       echo "TNG Path not found (" . __LINE__ . ") - ";
-   //  Display admin message if tng path not specified
-       add_action( 'admin_notices', array($this, 'pathNotSpecified') );
-       return;
-    }
-   return $configPath;
-}
-
-public function initTables()
-{
-    $configPath = $this->getConfigPath();
-    if (!file_exists($configPath)) {
-       //throw new DomainException('Could not find TNG config file');
-       $e = new DomainException('TNG Path not found');
-       error_log($e->getMessage());
-       error_log($e->getTraceAsString());
-       echo "TNG Path not found (" . __LINE__ . ")";
-   //  Display admin message if tng path not specified
-       add_action( 'admin_notices', array($this, 'pathNotSpecified') );
-       return;
-    }
-    include $configPath;
-    $vars = get_defined_vars();
-    foreach ($vars as $name => $value) {
-        if (preg_match('/_table$/', $name)) {
-            $this->tables[$name] = $value;
-        }
-        if (preg_match('/tngdomain$/', $name)) {
-            $this->domain = $value;
-        }
-    }
-}
-
-public function initVariables()
+    public function addShortcode(TngWp_Shortcode_AbstractShortcode $shortcode)
     {
-        //check for TNG Path
-        $configPath = $this->getConfigPath();
-        if (!file_exists($configPath)) {
-            $e = new Exception('TNG Path not found');
-            error_log($e->getMessage());
-            error_log($e->getTraceAsString());
-            echo "TNG Path not Specified";
-        //  Display admin message if tng path not specified
-            add_action( 'admin_notices', array($this, 'pathNotSpecified') );
-            return;
+        $this->shortcodes[] = $shortcode;
+    }
+
+    /**
+     * Init Variables
+    */    
+    public function initPlugin()
+    {
+        $templates = new TngWp_Templates();
+        foreach ($this->shortcodes as $shortcode) {
+            $shortcode->init($this, $templates);
         }
-        include $configPath;
-        //check Database
-        try {
-        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);  //DB error reporting
-        $db = @mysqli_connect($database_host, $database_username, $database_password, $database_name );
-        } catch (exception $e) {
+    }
+
+    public function getConfigPath() {
+        $configPath = ($this->getTngPath()) . DIRECTORY_SEPARATOR . "config.php";
+        //$configPath = $tngPath . DIRECTORY_SEPARATOR . "config.php";
+        if (!file_exists($configPath)) {
+        //throw new DomainException('Could not find TNG config file');
+        $e = new DomainException('TNG Path not found');
         error_log($e->getMessage());
         error_log($e->getTraceAsString());
-        echo "DB not configured. Please contact the administrator (80)";
-        add_action( 'admin_notices', array($this, 'dbNotSpecified') );
+        echo "TNG Path not found (" . __LINE__ . ") - ";
+    //  Display admin message if tng path not specified
+        add_action( 'admin_notices', array($this, 'pathNotSpecified') );
         return;
-        } 
-   
-    }
-
-public function getTngPath()
-{
-    $path = esc_attr(get_option('tng-api-tng-path'));
-    if (!file_exists($path)) {
-        add_action( 'admin_notices', array($this, 'pathNotSpecified'));
-    }
-    return $path;
-}
-public function getTngUrl()
-{
-    return esc_attr(get_option('tng-api-tng-url'));
-}
-
-public function getTngIntegrationPath()
-{
-    return esc_attr(get_option('tng-base-tng-path'));
-}
-public function getTngPhotoFolder()
-{
-    return esc_attr(get_option('tng-api-tng-photo-folder'));
-}
-public function getTngShowButtons()
-{
-    return esc_attr(get_option('tng-api-display-buttons'));
-}
-
-public function getTngTables()
-{
-    return $this->tables;
-}
-
-
-public function getDomain()
-{
-    return $this->domain;
-}
-
-public function guessVersion()
-{
-    $sql = 'describe ' . $this->tables['people_table'];
-    $sql2 = 'describe ' . $this->tables['users_table'];
-    if ($this->tables['image_tags_table']) {
-    $sql3 = 'describe ' . $this->tables['image_tags_table'];
-    $result3 = $this->query($sql3); 
-    }
-    $result = $this->query($sql);
-    $result2 = $this->query($sql2);
-    
-    $version = 9;
-    while ($row = $result->fetch_assoc()) {
-        if ($row['Field'] == 'burialtype') {
-            $version = 10;
-            break;
         }
-    }
-    while ($row = $result2->fetch_assoc()) {	
-        if ($row['Field'] == 'languageID') {
-            $version = 11;
-            break;
-        }
+    return $configPath;
     }
 
-    while ($row = $result2->fetch_assoc()) {	
-        if ($row['Field'] == 'dt_consented') {
-            $version = 12;
-            break;
-        }
-
-    }
-
-    if(isset($result3)) {
-    while ($row = $result3->fetch_assoc()) {	
-        if ($row['Field'] == 'ID') {
-            $version = 13;
-            break;
-        }
-    }
-
-    }
-
-    return $version;
-}
-
-public function query($sql)
-{
-    if (!$this->db) {
-        
-        throw new RuntimeException("No DB configured please contact the administrator(233)");
-    }
-    $result = mysqli_query($this->db, $sql);
-    if (!$result) {
-        throw new RuntimeException(mysqli_error($this->db));
-    }
-    return $result;
-}
-
-/**
- * @return mysqli
- */
-public function getDbLink()
-{
-    return $this->db;
-}
-
-public function init()
-{
-    if ($this->db) {
-        return $this;
-    }
-
-    if ($this->currentPerson) {
-        return $this;
-    }
-
-    // get_currentuserinfo();
-    
-        $dbHost = esc_attr(get_option('tng-api-db-host'));
-        $dbUser = esc_attr(get_option('tng-api-db-user'));
-        $dbPassword = esc_attr(get_option('tng-api-db-password'));
-        $dbName = esc_attr(get_option('tng-api-db-database'));
-        $EventID = esc_attr(get_option('tng-api-tng-event'));
-        try {
-            mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);  //DB error reporting
-            $db = mysqli_connect($dbHost, $dbUser, $dbPassword);
-            mysqli_select_db($db, $dbName);
-        } catch (exception $e) {
+    public function initTables()
+    {
+        $configPath = $this->getConfigPath();
+            if (!file_exists($configPath)) {
+            //throw new DomainException('Could not find TNG config file');
+            $e = new DomainException('TNG Path not found');
             error_log($e->getMessage());
             error_log($e->getTraceAsString());
-            echo "DB not configured. Please contact the administrator (203)";
+            echo "TNG Path not found (" . __LINE__ . ")";
+            //Display admin message if tng path not specified
+            add_action( 'admin_notices', array($this, 'pathNotSpecified') );
+            return;
+            }
+        include $configPath;
+        $vars = get_defined_vars();
+        foreach ($vars as $name => $value) {
+            if (preg_match('/_table$/', $name)) {
+                $this->tables[$name] = $value;
+            }
+            if (preg_match('/tngdomain$/', $name)) {
+                $this->domain = $value;
+            }
+        }
+    }
+
+    public function initVariables()
+        {
+            //check for TNG Path
+            $configPath = $this->getConfigPath();
+            if (!file_exists($configPath)) {
+                $e = new Exception('TNG Path not found');
+                error_log($e->getMessage());
+                error_log($e->getTraceAsString());
+                echo "TNG Path not Specified";
+            //  Display admin message if tng path not specified
+                add_action( 'admin_notices', array($this, 'pathNotSpecified') );
+                return;
+            }
+            include $configPath;
+            //check Database
+            try {
+            mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);  //DB error reporting
+            $db = @mysqli_connect($database_host, $database_username, $database_password, $database_name );
+            } catch (exception $e) {
+            error_log($e->getMessage());
+            error_log($e->getTraceAsString());
+            echo "DB not configured. Please contact the administrator (80)";
             add_action( 'admin_notices', array($this, 'dbNotSpecified') );
             return;
             } 
-       // mysqli_select_db($db, $dbName);
-        $this->db = $db; // added
-        $this->initTables(); 
+    
+    }
+    
+    public function getTngTables()
+    {
+        return $this->tables;
+    }
 
-        if (!isset($this->tables['users_table'])) { 
+    /*** Initialize *****/
+    public function init()
+    {
+        if ($this->db) { 
             return $this;
         }
 
-        
-        $tng_user_name = $this->getTngUserName();
-        $query = "SELECT * FROM {$this->tables['users_table']} WHERE username='{$tng_user_name}'";
-        $result = mysqli_query($db, $query);
-        if (!$result) {
-         //   throw new RuntimeException(mysqli_error($this->db));
+        if ($this->currentPerson) {
+            return $this;
         }
-        $row = $result->fetch_assoc();
-        $this->currentPerson = $row['personID'];
-        $this->db = $db;
-        return $this;
 
+        // get_currentuserinfo();
+        $configPath = $this->getConfigPath();
+        include $configPath;
+            try {
+                mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);  //DB error reporting
+                $db = mysqli_connect($database_host, $database_username, $database_password);
+                mysqli_select_db($db, $database_name);
+            } catch (exception $e) {
+                error_log($e->getMessage());
+                error_log($e->getTraceAsString());
+                echo "DB not configured. Please contact the administrator (203)";
+                add_action( 'admin_notices', array($this, 'dbNotSpecified') );
+                return;
+                } 
+        // mysqli_select_db($db, $dbName);
+            $this->db = $db; // added
+            $this->initTables(); 
+
+            if (!isset($this->tables['users_table'])) { 
+                return $this;
+            }
+
+            
+            $tng_user_name = $this->getTngUserName();
+            $query = "SELECT * FROM {$this->tables['users_table']} WHERE username='{$tng_user_name}'";
+            $result = mysqli_query($db, $query);
+            if (!$result) {
+            //   throw new RuntimeException(mysqli_error($this->db));
+            }
+            $row = $result->fetch_assoc();
+            $this->currentPerson = $row['personID'];
+            $this->db = $db;
+            return $this;
+
+        
+    }
     
-}
-
-public function getTngUserName()
-{
-    $user = $this->getTngUser();
-    return $user['username'];
-}
-
-
-
-
-    public function getCurrentPersonId()
+    /** tng root path *****/
+    public function getTngPath()
     {
-        return $this->currentPerson;
+        $rootPath = esc_attr(get_option('tng-api-tng-path'));
+        if (!file_exists($path)) {
+            add_action( 'admin_notices', array($this, 'pathNotSpecified'));
+        }
+        return $rootPath;
     }
 
+    /**TNG URL ****/
+    public function getTngUrl()
+    {
+        $configPath = $this->getConfigPath();
+        include $configPath;
+        return $tngdomain;
+    }
+
+    /**TNG WP Integration Path ****/
+    public function getTngIntegrationPath()
+    {
+        return esc_attr(get_option('tng-base-tng-path'));
+    }
+
+    /**TNG Photo Folder ****/
+    public function getTngPhotoFolder()
+    {
+        $configPath = $this->getConfigPath();
+        include $configPath;
+        return $photopath;
+    }
+
+    /** Show optional Buttons in Family Page ****/
+    public function getTngShowButtons()
+    {
+        return esc_attr(get_option('tng-api-display-buttons'));
+    }
+
+    public function query($sql)
+    {
+        if (!$this->db) {
+            
+            throw new RuntimeException("No DB configured please contact the administrator (" . __LINE__ . ") - ");
+        }
+        $result = mysqli_query($this->db, $sql);
+        if (!$result) {
+            throw new RuntimeException(mysqli_error($this->db));
+        }
+        return $result;
+    }
+
+    /**
+     * @return mysqli
+    */
+
+    public function getDbLink()
+    {
+        return $this->db;
+    }
+
+    /** Get TNG User Table */    
     public function getTngUser()
     {
         if ($this->tngUser) {
@@ -313,15 +258,25 @@ public function getTngUserName()
             $this->tngUser = $row;
             return $row;
         }
-        throw new Upavadi_WpOnlyException('User ' . $userName . ' not found in TNG (or donot have access to) (1247)'); //mu - change from wp_die
+        throw new Upavadi_WpOnlyException('User ' . $userName . ' not found in TNG (or donot have access (" . __LINE__ . ") - '); //mu - change from wp_die
     }
-/***
-** Birthdays **** 
-**/   
 
+    /** Get TNG User Name ****/
+    public function getTngUserName()
+    {
+        $user = $this->getTngUser();
+        return $user['username'];
+    }
+
+    public function getCurrentPersonId()
+    {
+        return $this->currentPerson;
+    }
+
+   
+    /*** Birthdays ******/   
     public function getBirthdays($month, $tree = null)
     {
-
         $user = $this->getTngUser();
         $gedcom = $user['gedcom'];
         // If we are searching, enter $tree value
@@ -334,34 +289,34 @@ public function getTngUserName()
         }
 
         $sql = <<<SQL
-SELECT personid,
-       firstname,
-       lastname,
-       birthdate,
-       birthplace,
-	   private,
-       famc,
-       gedcom
-FROM   {$this->tables['people_table']}
-WHERE  Month(birthdatetr) = {$month}
-       AND living = 1 {$treeWhere}
-ORDER  BY Day(birthdatetr),
-          lastname
-SQL;
-        $result = $this->query($sql);
+    SELECT personid,
+        firstname,
+        lastname,
+        birthdate,
+        birthplace,
+        private,
+        famc,
+        gedcom
+    FROM   {$this->tables['people_table']}
+    WHERE  Month(birthdatetr) = {$month}
+        AND living = 1 {$treeWhere}
+    ORDER  BY Day(birthdatetr),
+            lastname
+    SQL;
+            $result = $this->query($sql);
 
-        $rows = array();
-        while ($row = $result->fetch_assoc()) {
-            $userPrivate = $user['allow_private'];
-			$birthdayPrivate = $row['private'];
-			if ($birthdayPrivate > $userPrivate) {
-				$row['firstname'] = 'Private:';
-				$row['lastname'] = ' Details withheld';
+            $rows = array();
+            while ($row = $result->fetch_assoc()) {
+                $userPrivate = $user['allow_private'];
+                $birthdayPrivate = $row['private'];
+                if ($birthdayPrivate > $userPrivate) {
+                    $row['firstname'] = 'Private:';
+                    $row['lastname'] = ' Details withheld';
 
-			}
-			$rows[] = $row;
-        }
-		return $rows;
+                }
+                $rows[] = $row;
+            }
+        return $rows;
     }
     public function getFamily($personId = null, $tree = null)
     {
@@ -612,6 +567,52 @@ SQL;
         $rows[] = $row;
     }
     return $rows;
+}
+
+public function guessVersion()
+{
+    $sql = 'describe ' . $this->tables['people_table'];
+    $sql2 = 'describe ' . $this->tables['users_table'];
+    if ($this->tables['image_tags_table']) {
+    $sql3 = 'describe ' . $this->tables['image_tags_table'];
+    $result3 = $this->query($sql3); 
+    }
+    $result = $this->query($sql);
+    $result2 = $this->query($sql2);
+    
+    $version = 9;
+    while ($row = $result->fetch_assoc()) {
+        if ($row['Field'] == 'burialtype') {
+            $version = 10;
+            break;
+        }
+    }
+    while ($row = $result2->fetch_assoc()) {	
+        if ($row['Field'] == 'languageID') {
+            $version = 11;
+            break;
+        }
+    }
+
+    while ($row = $result2->fetch_assoc()) {	
+        if ($row['Field'] == 'dt_consented') {
+            $version = 12;
+            break;
+        }
+
+    }
+
+    if(isset($result3)) {
+    while ($row = $result3->fetch_assoc()) {	
+        if ($row['Field'] == 'ID') {
+            $version = 13;
+            break;
+        }
+    }
+
+    }
+
+    return $version;
 }
 } //End class
 ?>  
