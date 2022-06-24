5a1bbf81395f3dc4644bf58a29f1c7b95d134a86
upavadi <mahesh@upavadi.net>
2022-06-24 16:41:24 +0100

    Update

--- TngWp/Shortcode/MyShortcode.php
+++ TngWp/Shortcode/MyShortcode.php
@@ -7,18 +7,26 @@ class TngWp_Shortcode_MyShortcode extends TngWp_Shortcode_AbstractShortcode
     public function show()
     {
       $this->content->init();  
-      $personId = $this->content->getCurrentPersonId();
       
       
-        $context = array();
-        $tng_context = array();
-       $context['name'] = $this->content->getPersonName($personId);
-        $p_content = TngWp_PrivacyContent::instance(); //var_dump($p_content);
-        $context['x'] = $p_content->gethim(); //add function from Shortcode Content
-        $tng_context = $p_content->tngPrivacy(); //general privacy from PrivacyContent
-        $user_context = $p_content->userPrivacy(); //User privacy from PrivacyContent
-var_dump($tng_context, $user_context);
-        return $this->templates->render('myshortcode.html', $context);
+      
+        $content = array(); // shortcodeContent array
+        $p_content = array(); // privacyContent array
+        $content = TngWp_ShortcodeContent::instance(); 
+        $p_content = TngWp_PrivacyContent::instance(); 
+        $tng_content = $p_content->tngPrivacy(); //general privacy from PrivacyContent
+        $user_content = $p_content->userPrivacy(); //User privacy from PrivacyContent
+        $test_content = $p_content->testPrivacy(); //add function from Shortcode Content
+        
+        $requireLogin = $tng_content['tng_login'];
+        
+        //Sample array for html file.
+        $context = array(
+        'requireLogin' => $requireLogin  
+       );
+       
+       //This html file to show
+       return $this->templates->render('myshortcode.html', $context);
     }
 // No Privacy rules on this shortcode 
 }
