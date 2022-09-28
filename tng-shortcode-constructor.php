8868a0c1a5673c26e0cd95fd310c440ffffec1e9
upavadi <mahesh@upavadi.net>
2022-09-28 12:41:24 +0100

    random phot class added

--- tng-shortcode-constructor.php
+++ tng-shortcode-constructor.php
@@ -72,6 +72,7 @@ if (file_exists($tngPath)) {
 	$content->addShortcode(new TngWp_Shortcode_Danniversaries());
 	$content->addShortcode(new TngWp_Shortcode_Manniversaries());
 	$content->addShortcode(new TngWp_Shortcode_ManniversariesDeceased());
+	$content->addShortcode(new TngWp_Shortcode_RandomPhotoShortcode());
 	$content->addShortcode(new TngWp_Shortcode_LandingPage());
 	$content->addShortcode(new TngWp_Shortcode_Gone());
 	$content->addShortcode(new TngWp_Shortcode_TngSearch());
