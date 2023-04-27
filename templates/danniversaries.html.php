00c82ac7306de8b361065c091deac6627c3ab5cd
upavadi <mahesh@upavadi.net>
2023-04-27 13:06:18 +0100

    tidy up

--- templates/danniversaries.html.php
+++ templates/danniversaries.html.php
@@ -178,7 +178,5 @@ Clicking on a name takes you to the Individual's FAMILY Page.</br>
 </tbody>
 </table>
 </div>
-</div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-   
+</div>   
 </html>
